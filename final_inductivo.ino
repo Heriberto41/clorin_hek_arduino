eb551fd0535fa0df6a088ff52982c00b4cccd8a9
Heriberto41 <1715110167@utec-tgo.edu.mx>
2017-04-07 11:53:39 -0500

    Add files via upload

---
+++ final_inductivo.ino
@@ -0,0 +1,34 @@
+
+int sensorPin = 10;
+int led = 9;
+
+ 
+void setup()
+{
+   Serial.begin(9600);
+   Serial.println("inicio");
+  // digitalWrite(led, HIGH);
+  pinMode(led , OUTPUT);
+}
+ 
+void loop()
+{
+
+   bool state = digitalRead(sensorPin);
+ // Serial.println("Escaneando");
+   //mandar mensaje a puerto serie en función del valor leido
+   if (state == HIGH)
+   {
+      Serial.println("-si");
+      digitalWrite(led, HIGH);
+      
+      
+      //aquí se ejecutarían las acciones
+   }
+   else{
+     Serial.println("-no");
+     digitalWrite(led, LOW);
+   }
+   delay(500);
+   
+}
